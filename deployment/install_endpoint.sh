621449319f91863b357df487079c23d131a90f0b
baehless <38239600+baehless@users.noreply.github.com>
2018-12-10 14:33:31 +0100

    40 automatic endpoint deployment (#43)
    * adding binary download and permission changes
    
    * temp changes
    
    * fixed service file rewriting
    
    * concluded script with scion checks
    
    * added missing part
    
    * fixed problem with HTTP redirect and make binary executable
    
    * Some small enhacements

--- deployment/install_endpoint.sh
+++ deployment/install_endpoint.sh
@@ -1,40 +1,70 @@
 #!/bin/bash
 
+set -e # fail if unexpected error
+
+
 echo "Started installation of 2SMS Endpoint application"
 
-INSTALLATION_PATH=/home/scion/2SMS/deployment/endpoint
+# Defaults
+INSTALLATION_PATH="$HOME/2SMS/deployment/endpoint"
 DEFAULT_SOCKET=/run/shm/sciond/default.sock
-SERVICE_FILE=2SMSendpoint.service
+SERVICE_FILE_NAME=2SMSendpoint.service
+SERVICE_FILE_LOCATION=/etc/systemd/system
+MANAGER_IP='192.33.93.196'
+IP=$(curl -s ipinfo.io/ip)
+[ -f $SC/gen/ia ] && IA=$(cat $SC/gen/ia | sed 's/_/:/g') || { echo "Missing $SC/gen/ia file"; exit 1; }
 
 # Prepare filesystem
-echo 'Creating installation directory at $INSTALLATION_PATH'
+echo "Creating installation directory at $INSTALLATION_PATH"
 mkdir -p $INSTALLATION_PATH
 cd $INSTALLATION_PATH
 
 # Download binary file
 echo "Downloading binary"
-# TODO: gist on github as Juan suggested
-# wget
+rm -f endpoint
+wget https://gist.github.com/baehless/0af8c4fca2db16737a6e31b7e725ad98/raw/d40277423a542979b2f83ffd5638c9e8392eaf6a/endpoint -O endpoint
+# Make executable
+chmod +x endpoint
 
 # Download service file
-echo "Downloading service files"
-wget https://raw.githubusercontent.com/netsec-ethz/2SMS/master/endpoint/2SMSendpoint.service
-# TODO: Modify service file with correct SCION address and IP parameters
-sudo mv SERVICE_FILE /etc/systemd/system/
-# TODO: chown, chgrp, chmod?
+echo "Downloading service file"
+rm -f $SERVICE_FILE_NAME
+wget https://raw.githubusercontent.com/netsec-ethz/2SMS/master/endpoint/2SMSendpoint.service -O $SERVICE_FILE_NAME
+
+# Modify service file with correct SCION address and IP parameters
+sed -i -r "s/_USER_/$USER/g;s/_MANAGER_IP_/$MANAGER_IP/g;s/^(.+)_IA_,\[_IP_\]:9199 -endpoint\.IP _IP_(.+)$/\1$IA,[$IP]:9199 -endpoint.IP $IP\2/g" $SERVICE_FILE_NAME
+
+# Move service file to the right place
+sudo mv $SERVICE_FILE_NAME /etc/systemd/system/
+sudo chown root "$SERVICE_FILE_LOCATION/$SERVICE_FILE_NAME"
+sudo chgrp root "$SERVICE_FILE_LOCATION/$SERVICE_FILE_NAME"
 
 # Check system
-[ ! -v $SC ] || [ ! -d $SC ] && { echo "SCION environment variable $SC not set, is SCION installed correctly?"; exit 1 }
-[ ! -s $DEFAUL_SOCKET ] && { echo 'SCION default socket not found at $DEFAULT_SOCKET'; exit 1 }
+echo "Checking SCION installation"
+if [ -z $SC ]; then
+    echo 'SCION environment variable $SC not set, is SCION installed correctly?'
+    exit 1
+elif [ ! -d $SC/gen/ISD*/AS*/endhost/certs ]; then
+    echo "SCION endhost's cert directory not found. Aborting."
+    exit 1
+elif [ ! -S $DEFAULT_SOCKET ]; then
+    echo "SCION default socket not found at $DEFAULT_SOCKET"
+    exit 1
+else
+    echo "SCION correctly installed"
+fi
 
 # Start service
-sudo systemctl start $SERVICE_FILE
+echo "Starting $SERVICE_FILE_NAME"
+sudo systemctl stop $SERVICE_FILE_NAME || true
+sudo systemctl daemon-reload
+sudo systemctl start $SERVICE_FILE_NAME
+sudo systemctl enable $SERVICE_FILE_NAME || true # at boot time
 
 # Check service status
-# TODO: check output or status code from service
-if [ ]; then
-	echo "Successfully installed and started 2SMS Endpoint application"
-else; then
-	echo 'Failed starting 2SMS Endpoint application, something went wrong during installation'
-	exit 1
+if systemctl is-active --quiet 2SMSendpoint.service; then
+    echo "Successfully installed and started 2SMS Endpoint application"
+else
+    echo 'Failed starting 2SMS Endpoint application, something went wrong during installation. Check journal and syslog to have more details.'
+    exit 1
 fi
